2c1c6df154bbab40cec0e4ec12199b009de216db
shreyas-cell <69566823+shreyas-cell@users.noreply.github.com>
2022-09-14 17:59:22 +0530

    Create AlphabetPattern.java

---
+++ src/AlphabetPattern.java
@@ -0,0 +1,12 @@
+public class AlphabetPattern {
+public static void main(String[] args) {
+	
+	for(int i=65;i<=90;i++) {
+		for(int j=65;j<=i;j++) {
+		System.out.print((char)j);	
+		}
+		System.out.println();
+	}
+	
+}
+}
