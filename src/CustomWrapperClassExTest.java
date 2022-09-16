72f819b9bfa99bea16615b73078b6997a3c4857d
shreyas-cell <69566823+shreyas-cell@users.noreply.github.com>
2022-09-16 18:41:25 +0530

    Create CustomWrapperClassExTest.java

---
+++ src/CustomWrapperClassExTest.java
@@ -0,0 +1,8 @@
+package com.p;
+
+public class CustomWrapperClassExTest {
+public static void main(String[] args) {
+	CustomWrapperClassEx c=new CustomWrapperClassEx(10);  
+	System.out.println(c);  
+}
+}
